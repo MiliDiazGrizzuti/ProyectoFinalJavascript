f69321e3aa916d20de2f255273a31d2cd0745d91
MiliDiazGrizzuti <milagros.diazgrizzuti@hotmail.com>
2022-08-23 20:13:03 -0300

    Creacion del proyecto

---
+++ index.js
@@ -0,0 +1,18 @@
+// Const EDAD = 15 (Esto sería una variable que no va a cambiar nunca)
+
+// Todo lo que no tenga un valor real, es decir, que no sea un número va entre "" (string)
+
+// Camel Case = el segundo nombre de la variable va en mayúscula, ejemplo: numeroUno
+
+// console.log() nos ayuda a mostrar en la consola que es lo que se ejecuta (con el coderunner instalado)
+
+// prompt = le solicita información al usuario
+
+// alert = muestra la información al usuario
+
+// Baltiks = gracias a esto podemos colocar la variable en el texto. Ejemplo:
+// let saludo = "Hola"
+// let nombre = "Andres"
+// let mensaje = `${saludo} ${nombre}, gracias por visitarnos ♥´;
+
+// 
