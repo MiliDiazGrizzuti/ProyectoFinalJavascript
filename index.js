563593573a1542ecb55bb0f1906b0b98d3fd19c6
MiliDiazGrizzuti <milagros.diazgrizzuti@hotmail.com>
2022-09-11 19:36:25 -0300

    Usando dom

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ class Persona {
     }
 }
 
+//VARIABLES GLOBALES
+let personas = [];
 
 //FUNCIONES
 let opcionInvalida = (opcion) => opcion.toLowerCase() !== "si" && opcion.toLocaleLowerCase() !== "no";
@@ -27,13 +29,68 @@ let promedio = (array) => {
     return 0;
 };
 
+let limpiarDatos = () => {
+    let nombreInput = document.getElementById("nombre");
+    let edadInput = document.getElementById("edad");
+    let sintomasInput = document.getElementById("sintomas");
+    nombreInput.value = null;
+    edadInput.value = null;
+    sintomasInput.value = null;
+};
 
-//VARIABLES GLOBALES
-let personas = [];
+let procesarDatos = () => {
+    let nombreInput = document.getElementById("nombre");
+    let edadInput = document.getElementById("edad");
+    let sintomasInput = document.getElementById("sintomas");
+    let nombre = nombreInput.value;
+    let edadValue = edadInput.value;
+    let sintomas = sintomasInput.value;
 
+    // Validar los datos ingresados
+    if(edadInvalida(edadValue) || opcionInvalida(sintomas)) {
+        alert("Datos erróneos");
+        return;
+    }
+
+    // Agregarlo al array de Personas
+    let edad = parseInt(edadValue);
+    let conSintomas = sintomas.toLowerCase() === "si";
+    let index = personas.length + 1;
+    let persona = new Persona (index, nombre, edad, conSintomas);
+    personas.push(persona);
+    alert("Persona agregada");
+    
+    let pPersonasAgregadas = document.getElementById("pPersonasAgregadas");
+    pPersonasAgregadas.innerHTML = `<p>Cantidad de personas agregadas: ${index}</p>`;
+    
+    if(persona.personaDeRiesgo()){
+        let pPersonasConRiesgo = document.getElementById("pPersonasConRiesgo");
+        pPersonasAgregadas.append(`${persona.nombre} tiene riesgo de covid`)
+    }
+    
+    limpiarDatos();
+};
+
+let calcularPromedio = () => {
+    if(personas.length){
+        let edades = personas.map (item => item.edad);
+        let promedioEdades = promedio(edades);
+        let mensaje = `El promedio de edades es de ${promedioEdades}`;
+        alert(mensaje);    
+    }
+    else {
+        alert("Ingrese personas para calcular promedio de edades");
+    }
+};
+
+//EVENTOS
+let btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", procesarDatos);
+let btnPromedio = document.getElementById("btnPromedio");
+btnPromedio.addEventListener("click", calcularPromedio);
 
 //INGRESO DE DATOS
-for (let i = 1; i < 5; i++){
+/*for (let i = 1; i < 5; i++){
     let nombre = prompt ("Ingrese su nombre");
     
     let edad = prompt ("Ingrese su edad");
@@ -63,3 +120,4 @@ alert(mensaje);
 let pacientesDeRiesgo = personas.filter (item => item.personaDeRiesgo());
 let nombresPacientesDeRiesgo = pacientesDeRiesgo.map(item => item.nombre).join(", ");
 alert(`${nombresPacientesDeRiesgo} son pacientes de riesgo`);
+*/
